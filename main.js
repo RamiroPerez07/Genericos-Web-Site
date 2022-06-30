584ee93df718be98bebf93d61811f1ea26f1e75e
Ramiro Perez <ramiperez1997@hotmail.com>
2022-06-29 23:54:06 -0300

    agregada barra de busqueda

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById("btn-main-search").addEventListener("click",function(){
+    alert("El boton esta funcionando. Estos son mis primeros pasos en javascript. Hola mundooo");
+});
