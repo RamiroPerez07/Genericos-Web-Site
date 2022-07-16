0c88fa62fb55bc25b09e3fede8b22e46c1576180
Ramiro Perez <ramiperez1997@hotmail.com>
2022-07-16 13:13:55 -0300

    corregidas las interacciones con el menu hamburguesa

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 document.getElementById("btn-main-search").addEventListener("click",function(){
     alert("El boton esta funcionando. Estos son mis primeros pasos en javascript. Hola mundooo");
 });
+
+document.querySelector("#nav-menu").addEventListener("click", () => {
+    console.log("esto funciona");
+    document.querySelector("#nav-menu").classList.toggle("show");
+  });
+
+
+  document.querySelector("#check-toggle").addEventListener("click", () => {
+    console.log("esto funciona");
+    document.querySelector("#nav-menu").classList.toggle("show");
+  });
+
+
+let scrollPrevio = window.scrollY;
+window.onscroll = function () {
+    let scrollActual = window.scrollY;
+    if (scrollPrevio > scrollActual) {
+        document.getElementById("main-header").style.top = "0";
+    } else {
+        document.getElementById("main-header").style.top = "-100%";
+    }
+    scrollPrevio = scrollActual;
+}
+
+
+
+
+
+
